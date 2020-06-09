d0d1673a00ddf5a4e5e7fd92bcaeb2773f563067
Joseph Morrison <jmorri35@gmail.com>
2020-06-09 21:22:58 +0100

    Add layout items / global items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './global/header/header.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { LayoutComponent } from './common/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
